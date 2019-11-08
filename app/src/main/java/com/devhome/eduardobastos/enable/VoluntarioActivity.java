4cd11cb7190c553b5660c64bc5e7543fa3ab4e4a
edudevhome <com.devhome.eduardobastos@gmail.com>
2019-11-08 18:46:47 -0300

    Tela inicial (done). Formulário Voluntário(done). Formulário Paciente(pending).

--- app/src/main/java/com/devhome/eduardobastos/enable/VoluntarioActivity.java
+++ app/src/main/java/com/devhome/eduardobastos/enable/VoluntarioActivity.java
@@ -3,12 +3,17 @@ package com.devhome.eduardobastos.enable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
 
 public class VoluntarioActivity extends AppCompatActivity {
+
+    private ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voluntario);
+
+
     }
 }
