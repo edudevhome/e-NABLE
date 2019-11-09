ffc20d0db6109f4fa3fcf13f2f5a1ea69eab8cb1
edudevhome <com.devhome.eduardobastos@gmail.com>
2019-11-09 16:13:25 -0300

    Tela inicial (done). Formulário Voluntário(done). Formulário Paciente(done).

--- app/src/main/java/com/devhome/eduardobastos/enable/PacienteActivity.java
+++ app/src/main/java/com/devhome/eduardobastos/enable/PacienteActivity.java
@@ -3,12 +3,51 @@ package com.devhome.eduardobastos.enable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class PacienteActivity extends AppCompatActivity {
+
+    private Button buttonPaciente;
+    private Spinner spinnerEstados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paciente);
+
+
+
+        buttonPaciente = findViewById(R.id.buttonEnviarPac);
+        spinnerEstados = findViewById(R.id.spinnerEstados);
+
+        carregarDadosSpinner();
+
+        buttonPaciente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Cadastro realizado com sucesso!", Snackbar.LENGTH_LONG)
+                        .show();
+            }
+        });
+    }
+
+
+    private void carregarDadosSpinner() {
+
+
+        //configura Spinner de Estados
+        String[] estados = getResources().getStringArray(R.array.estados);
+        ArrayAdapter<String> adapterE = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item,
+                estados
+        );
+        adapterE.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerEstados.setAdapter(adapterE);
     }
 }
+
--- app/src/main/java/com/devhome/eduardobastos/enable/VoluntarioActivity.java
+++ app/src/main/java/com/devhome/eduardobastos/enable/VoluntarioActivity.java
@@ -2,18 +2,55 @@ package com.devhome.eduardobastos.enable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ScrollView;
+import android.widget.Spinner;
+
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class VoluntarioActivity extends AppCompatActivity {
 
-    private ScrollView scrollView;
+    private Button buttonEnviar;
+    private Spinner spinnerEstados;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_voluntario);
 
+        buttonEnviar = findViewById(R.id.buttonEnviarVol);
+        spinnerEstados = findViewById(R.id.spinnerEstados);
 
+        carregarDadosSpinner();
+
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Snackbar.make(view, "Cadastro realizado com sucesso!", Snackbar.LENGTH_LONG)
+                        .show();
+
+
+            }
+        });
+
+    }
+
+    private void carregarDadosSpinner() {
+
+        //configura Spinner de Estados
+        String[] estados = getResources().getStringArray(R.array.estados);
+        ArrayAdapter<String> adapterE = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item,
+                estados
+        );
+        adapterE.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerEstados.setAdapter(adapterE);
     }
 }
